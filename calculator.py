224489733f03565d439eb8cfdc084ff55a4e80e7
zonex <igorkotlik1@gmail.com>
2025-06-29 18:52:03 +0200

    Refactor calculator.py to encapsulate user input and operation logic within the main block, added tests

--- calculator.py
+++ calculator.py
@@ -4,19 +4,20 @@ def add(a, b):
 def substract(a, b):
     return a - b
 
-num_1 = int(input("Enter first number: "))
-num_2 = int(input("Enter second number: "))
+if __name__ == "__main__":
+    num_1 = int(input("Enter first number: "))
+    num_2 = int(input("Enter second number: "))
 
-print("Possible operations:")
-print("a. add")
-print("b. substract")
-operation = input("Enter operation(a/b): ")
+    print("Possible operations:")
+    print("a. add")
+    print("b. substract")
+    operation = input("Enter operation(a/b): ")
 
-match operation:
-    case 'a':
-        print(add(num_1,num_2))
-    case 'b':
-        print(substract(num_1,num_2))
+    match operation:
+        case 'a':
+            print(add(num_1,num_2))
+        case 'b':
+            print(substract(num_1,num_2))
 
 
 
